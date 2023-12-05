523511e09eef0f4a4870ffcd0dd63dc4fb397bc7
GZ <GZ@GZ_PC>
2023-12-05 08:21:05 +0200

    Displaying a List of Products

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            return View(repository.Products);
         }
     }
 }

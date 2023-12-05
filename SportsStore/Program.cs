d3d9cb9b9748bc323899b4097ae57000aa63ed69
GZ <GZ@GZ_PC>
2023-12-05 06:55:16 +0200

    Preparing the Application Services and the Request Pipeline The Program

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();//method sets up the shared objects required by applications using the MVC Framework and the Razor view engine.
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
 app.Run();

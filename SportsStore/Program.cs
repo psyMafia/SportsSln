fbb3e2d8c5fc523db6681ef6752eb9662428db2e
GZ <GZ@GZ_PC>
2023-12-05 07:47:00 +0200

    Defining the Connection String

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();//method sets up the shared objects required by applications using the MVC Framework and the Razor view engine.
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
